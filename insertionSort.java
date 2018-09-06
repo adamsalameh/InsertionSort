c033bb0002f9ab40a9280c060ede529d19c4d660
Adam <micro.83@hotmail.com>
2018-09-06 11:35:04 +0300

    Update insertionSort.java

--- insertionSort.java
+++ insertionSort.java
@@ -23,8 +23,24 @@ class Rextester
         }
      return array;
     }
-    
-    
+   /*   public static int[] insertionSort (int[] array){
+        
+        for (int i = 1; i<array.length; i++){
+            
+            int key = array[i];
+            
+            int j = i -1;
+            
+            for(; j >= 0 && array[j] > key; j--){
+                
+                array[j + 1 ] = array[j];
+              
+            }
+                 array[j+1 ] = key;  
+                            
+            }  
+         return array;
+    }   */
     
     public static void main(String args[])
     {
