dc4c2772748a21a923068175e7fb12b8a55025cc
Adam <micro.83@hotmail.com>
2019-08-08 20:27:02 +0300

    Update insertionSort.php

--- insertionSort.php
+++ insertionSort.php
@@ -1,32 +1,25 @@
 <?php //php 7.0.8
- //                       0  1    2   3   4   5   6   7   8   9  10  11  12  13  14  15 16 17 18 19  20  21  22  23  24
-        $numbers = array(29, 31, 37, 41, 79, 83, 89, 43, 47, 53, 59, 61, 67, 71, 73, 2, 3, 5, 7, 11, 13, 17, 19, 23, 97);
-       
  
-       
-        function insertionSort($array) {  // To effect the orginal array pass it by reference &$array
-    
-    for ($i = 1; $i < count($array); $i++)
-    {
+// To effect the orginal array pass it by reference &$array       
+function insertionSort($array)
+{
+    for ($i = 1; $i < count($array); $i++) {
         $key = $array[$i];
-        $j = $i-1;
-     
-        while ($j >= 0 && $array[$j] > $key)
-        {
+        $j = $i - 1;     
+        while ($j >= 0 && $array[$j] > $key) {
             $array[$j + 1] = $array[$j];
             $j--;
-        }
-         
+        }         
         $array[$j + 1] = $key;
     }
-        return $array;    
+    return $array;    
 }
-  
-      
-    print_r($numbers);
-    echo "The array after the insertion sort\n";
-    print_r(insertionSort($numbers));
 
-  
+//                0  1    2   3   4   5   6   7   8   9  10  11  12  13  14  15 16 17 18 19  20  21  22  23  24
+$numbers = array(29, 31, 37, 41, 79, 83, 89, 43, 47, 53, 59, 61, 67, 71, 73, 2, 3, 5, 7, 11, 13, 17, 19, 23, 97); 
+      
+print_r($numbers);
+echo "The array after the insertion sort\n";
+print_r(insertionSort($numbers)); 
     
 ?>
